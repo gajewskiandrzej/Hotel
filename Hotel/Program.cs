36cd3ce672371c80018462c24f02d87a7be04df8
Andrzej Gajewski <gajewskiandrzej@hotmail.com>
2018-11-18 20:01:08 +0100

    prawie koniec

--- Hotel/Program.cs
+++ Hotel/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,130 +11,159 @@ namespace Hotel
 
     public abstract class Rezerwacja
     {
-        public int pokoj;
-
-        public abstract bool WyszukajWolnyPokoj(StanPokoju stanPokoju);
-        public abstract void ZarezerwujPokoj(ref int pokoj);
+        public void WyszukajWolnyPokoj()
+        {
+            foreach (Pokoj pokoj in Hotel.pokoje)
+            {
+                if (pokoj.StanPokoju == StanPokoju.wolny)
+                {
+                    Console.WriteLine("Wolny pokoj: " + pokoj.NrPokoju);
+                }
+                else
+                {
+                    Console.WriteLine("Brak wolnych pokoi");
+                }
+            }
+        }
+        public void ZarezerwujPokoj(byte nrPokoju, Gosc gosc)
+        {
+            foreach (Pokoj pokoj in Hotel.pokoje)
+            {
+                if(pokoj.NrPokoju == nrPokoju)
+                {
+                    pokoj.StanPokoju = StanPokoju.zarezerwowany;
+                }
+            }
+        }
     }
 
     public class Pokoj
     {
-        int nrPokoju { get; set; }
-        string nazwiskoGoscia;
-        public string NazwiskoGoscia
-        {
-            get
-            {
-                return nazwiskoGoscia;
-            }
-            set
-            {
-                nazwiskoGoscia = value;
-            }
-        }
-        StanPokoju stanPokoju = StanPokoju.wolny;
+        public byte NrPokoju { get; set; }
+        public string NazwiskoGoscia { get; set; }
+        public StanPokoju StanPokoju { get; set; }
 
-        public Pokoj(int nrPokoju)
+        public Pokoj(byte nrPokoju, string nazwiskoGoscia, StanPokoju stanPokoju)
         {
-            this.nrPokoju = nrPokoju;
+            NrPokoju = nrPokoju;
+            NazwiskoGoscia = nazwiskoGoscia;
+            StanPokoju = stanPokoju;
         }
 
-        public void Zarezerwuj(string nazwiskoGoscia)
+        public Pokoj()
         {
-            stanPokoju = StanPokoju.zarezerwowany;
+            NrPokoju = 0;
+            NazwiskoGoscia = string.Empty;
+            StanPokoju = StanPokoju.wolny;
+        }
+
+        public void Zarezerwuj(byte nrPokoju, string nazwiskoGoscia)
+        {
+            NrPokoju = nrPokoju;
+            StanPokoju = StanPokoju.zarezerwowany;
             NazwiskoGoscia = nazwiskoGoscia;
         }
 
-        public void Wydaj()
+        public void Wydaj(byte nrPokoju, string nazwiskoGoscia)
         {
-            stanPokoju = StanPokoju.zajety;
+            NrPokoju = nrPokoju;
+            StanPokoju = StanPokoju.zajety;
             NazwiskoGoscia = nazwiskoGoscia;
         }
 
-        public void Przyjmij()
+        public void Przyjmij(byte nrPokoju)
         {
-            stanPokoju = StanPokoju.wolny;
+            NrPokoju = nrPokoju;
+            StanPokoju = StanPokoju.wolny;
             NazwiskoGoscia = string.Empty;
         }
 
-        public void Wycofaj()
+        public void Wycofaj(byte nrPokoju)
         {
-            stanPokoju = StanPokoju.wycofany;
+            NrPokoju = nrPokoju;
+            StanPokoju = StanPokoju.wycofany;
+            NazwiskoGoscia = string.Empty;
         }
 
         public override string ToString()
         {
-            return String.Format("Numer pokoju: {0} Nazwisko gościa: {1} Stan pokoju: {2}", nrPokoju, nazwiskoGoscia, stanPokoju);
+            return String.Format("Numer pokoju: {0} Stan pokoju: {1} Nazwisko gościa: {2}", NrPokoju, StanPokoju, NazwiskoGoscia);
         }
     }
 
-    class Gosc : Rezerwacja
+    public class Hotel
     {
-        string nazwisko;
-        public string Nazwisko { get; set; }
+        public static Queue<Pokoj> pokoje = new Queue<Pokoj>();
 
+        public void PokazPokoje()
+        {
+            foreach (Pokoj pokoj in pokoje)
+            {
+                Console.WriteLine(pokoj);
+            }
+        }
+    }
+
+
+    public class Gosc : Rezerwacja
+    {
+        public string Nazwisko { get; set; }
+    
         public Gosc(string nazwisko)
         {
-            this.nazwisko = nazwisko;
-        }
-
-        public override bool WyszukajWolnyPokoj(StanPokoju stanPokoju)
-        {
-            if (stanPokoju == StanPokoju.wolny)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        public override void ZarezerwujPokoj(ref int nrPokoju)
-        {
-            StanPokoju stanPokoju = StanPokoju.zarezerwowany;
             Nazwisko = nazwisko;
         }
-    }
 
-    class Hotel
-    {
-
-    }
-
-    class DyrektorHotelu
-    {
-        public static void DodajPokoj()
+        public Gosc()
         {
-
-        }
-
-        public static void WycofajPokoj()
-        {
-
+            Nazwisko = string.Empty;
         }
     }
 
-    class Recepcjonista
+
+    public class DyrektorHotelu
     {
-        public static void WyszukajWolnyPokoj()
+        public void DodajPokoj(Pokoj pokoj)
         {
-
+            Hotel.pokoje.Enqueue(pokoj);
         }
 
-        public static void WyszukajZarezerwowany()
+        public void WycofajPokoj(byte nrPokoju)
         {
+            foreach (Pokoj pokoj in Hotel.pokoje)
+            {
+                if(pokoj.NrPokoju == nrPokoju)
+                {
+                    pokoj.StanPokoju = StanPokoju.wycofany;
+                }
+            }
+        }
+    }
 
+    class Recepcjonista : Rezerwacja
+    {
+        public void WyszukajZarezerwowany()
+        {
+            foreach (Pokoj pokoj in Hotel.pokoje)
+            {
+                if (pokoj.StanPokoju == StanPokoju.zarezerwowany)
+                {
+                    Console.WriteLine("Zarezerwowany pokoj: " + pokoj.NrPokoju + " Nazwisko gościa" + pokoj.NazwiskoGoscia);
+                }
+            }
         }
 
-        public static void ZarezerwujPokoj()
+        public void ZwolnijPokoj(byte nrPokoju)
         {
-
-        }
-
-        public static void ZwolnijPokoj()
-        {
-
+            foreach (Pokoj pokoj in Hotel.pokoje)
+            {
+                if (pokoj.NrPokoju == nrPokoju)
+                {
+                    pokoj.NrPokoju = nrPokoju;
+                    pokoj.StanPokoju = StanPokoju.wolny;
+                    pokoj.NazwiskoGoscia = string.Empty;
+                }
+            }
         }
     }
 
@@ -141,15 +171,42 @@ namespace Hotel
     {
         static void Main(string[] args)
         {
-            Pokoj pokoj1 = new Pokoj(1);
-            Pokoj pokoj2 = new Pokoj(2);
-            pokoj1.Zarezerwuj("Gajewski");
 
-            Gosc gosc1 = new Gosc("Nowak");
-            gosc1.ZarezerwujPokoj(2);
+            Hotel hotel = new Hotel();
+            Pokoj pokoj1 = new Pokoj(1, "", StanPokoju.wolny);
+            Pokoj pokoj2 = new Pokoj(2, "", StanPokoju.wolny);
+            Pokoj pokoj3 = new Pokoj(3, "", StanPokoju.wolny);
+            Pokoj pokoj4 = new Pokoj(4, "", StanPokoju.wolny);
+            Pokoj pokoj5 = new Pokoj(5, "", StanPokoju.wolny);
 
-            Console.WriteLine(pokoj1);
-            Console.WriteLine(pokoj2);
+            DyrektorHotelu dyrektorHotelu = new DyrektorHotelu();
+
+            dyrektorHotelu.DodajPokoj(pokoj1);
+            dyrektorHotelu.DodajPokoj(pokoj2);
+            dyrektorHotelu.DodajPokoj(pokoj3);
+            dyrektorHotelu.DodajPokoj(pokoj4);
+            dyrektorHotelu.DodajPokoj(pokoj5);
+
+            Recepcjonista recepcjonistkaGosia = new Recepcjonista();
+
+            Console.WriteLine("Otwarcie hotelu");
+            Console.WriteLine("=================================");
+            hotel.PokazPokoje();
+
+            Gosc gosc1 = new Gosc("Gajewski");
+            Gosc gosc2 = new Gosc("Wiśniewski");
+            Gosc gosc3 = new Gosc("Nowak");
+            Gosc gosc4 = new Gosc("Kowalski");
+
+            Console.WriteLine("Lista wolnych pokoi:");
+            Console.WriteLine("=================================");
+            gosc1.WyszukajWolnyPokoj();
+            gosc1.ZarezerwujPokoj(1, gosc1);
+
+            Console.WriteLine("Lista zarezerwowanych pokoi:");
+            Console.WriteLine("=================================");
+            recepcjonistkaGosia.WyszukajZarezerwowany();
+
 
             Console.ReadKey();
         }
